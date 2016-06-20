0627490f9eb6378d2fef72ffefc7c2332c37ef3d
PatriciaBoehringer <patricia.boehringer@gmx.de>
2016-06-20 12:24:28 +0200

    Ronja&Patricia bisherige Arbeit

---
+++ frameworkpoerksi.pl
@@ -0,0 +1,20 @@
+% PROLOG
+
+:- include('read.pl'). % uses file read.pl
+
+% start / exit the game
+poerksi :- read_sentence(Input), poerksi(Input),!.
+poerksi([tschüss]) :- writeln('Ich hoffe ich konnte dir helfen. Viel Spaß beim studieren! Und du weißt ja, falls du Fragen hast, wo man micht findet.'). %
+
+% User-Input (after program is started)
+poerksi(Input) :-
+  match(Input,Output), % match user input (define match in poerksi.pl)
+  reply(Output),
+  read_sentence(Input1),!,
+  poerksi(Input1).
+
+% Output after User-Input
+reply([Head|Tail]) :- write(user_output,Head), write(user_output,' '),
+	               reply(Tail).
+reply([]) :- nl.
+
