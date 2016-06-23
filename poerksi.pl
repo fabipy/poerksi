bd681bcf010ec27d6607ededc123fe2d15ff8725
RonjaBoehringer <ronja.boehringer@web.de>
2016-06-23 22:19:47 +0200

    Mehr Varianten zur Begrüßung, Frage: Was kann man damit machen eingepflegt, ein paar Beleidigungen eingefügt, Allgemeine Infos zum Studium

--- poerksi.pl
+++ poerksi.pl
@@ -25,7 +25,7 @@ ansprechpartner('Fachschaft Medienwissenschaft','fachschaft@medienwissenschaft.u
 
 %ansprechp/0 
 % Gibt Liste von Ansprechpartnern aus
-ansprechp:- bagof(Name,ansprechpartner(Name,_,_,_),X), write(X).
+ansprechp :- setof(Name,ansprechpartner(Name,_,_,_),X), write(X).
 
 %Basisstudium
 
@@ -82,8 +82,21 @@ read_name(NewName) :-
 % Smalltalk
 match([hallo],['Hallo, mein Name ist Dr. Poerksi und ich bin der virtuelle Studi-Ratgeber des Instituts für Medienwissenschaft.
 Stellen Sie mir einfach Ihre Fragen und ich werde versuchen Ihnen dabei zu helfen.']).
+match([guten,tag],['Hallo, mein Name ist Dr. Poerksi und ich bin der virtuelle Studi-Ratgeber des Instituts für Medienwissenschaft.
+Stellen Sie mir einfach Ihre Fragen und ich werde versuchen Ihnen dabei zu helfen.']).
+match([hi],['Hallo, mein Name ist Dr. Poerksi und ich bin der virtuelle Studi-Ratgeber des Instituts für Medienwissenschaft.
+Stellen Sie mir einfach Ihre Fragen und ich werde versuchen Ihnen dabei zu helfen.']).
+match([servus],['Hallo, mein Name ist Dr. Poerksi und ich bin der virtuelle Studi-Ratgeber des Instituts für Medienwissenschaft.
+Stellen Sie mir einfach Ihre Fragen und ich werde versuchen Ihnen dabei zu helfen.']).
+
 match([ich,heisse,X],['Schön dich kennenzulernen',X,'Ein herzliches Willkommen unter uns Medienwissenschaftlern!']) :- name("Gast"),retract(name(_)), assert(name(X)).
 match([ich,heisse,N],['Dann heißt du jetzt eben:',L]) :- assertz(name(N)), findall(B,name(B),L).
+
+%Beleidigungen
+match([fick,dich],['So eine Ausdrucksweise verbitte ich mir. Ich glaube Sie sind nicht für das Studium der Medienwissenschaft geeignet.']).
+match([arschloch],['Es ist mir ein Rätsel wie so ein minderbemitteltes Wesen wie Sie das Abitur geschafft hat.']).
+match([wichser],['Dies ist nicht der Ort für Beleidigungen.']).
+match([du,_],['Für Sie immer noch SIE. Ich habe Ihnen niemals das DU angeboten.']).
 
 % auf read_name verweisen und namen speichern / 23.06.16 FL | muss noch getan werden
 match([wie,heisse,ich],['Ich kenne deinen Namen leider noch nicht. Wie heißt du denn?']) :- name("Gast").
@@ -92,9 +105,40 @@ match([wie,heisse,ich],['Sie haben Ihren eigenen Namen vergessen? Ich kann Ihnen
 %Anfrage: Wie ist mein Name..
 match([wie, geht, es, dir],['Mir geht es sehr gut. Schließlich bin ich ja ein angesehener Professor, der sich in den unterschiedlichsten Medienbereichen bestens auskennt.']).
 match([was,geht],['Normalerweise ist mir so ein Sprachgebrauch nicht geläufig aber läuft bei mir. Und was geht bei Ihnen?']).
+match([was,geht,ab],['Normalerweise ist mir so ein Sprachgebrauch nicht geläufig aber läuft bei mir. Und was geht bei Ihnen?']).
 match([do,you,speak,english],['Yes, I do. Jedoch möchte ich meine Gehirnkapazität mit der höchst möglichen Aktivität nutzen, daher bleibe ich lieber bei meiner Muttersprache. Das geht einfach schneller und ich kann dir mehr von meinem Wissen weiter geben.']).
 %Anfrage: was kann man später damit machen?
+match([was,kann,_,spaeter,damit,machen],['Durch dieses sehr empfehlenswerte Studium werden Sie auf folgende Berufsfelder vorbereitet:
+	Öffentlichkeitsarbeit,
+	(Fach)Journalismus,
+	Verlage,
+	Markt-und Medienforschung,
+	Medienkonzeption und -produktion
+	Medienmanagement,
+	und öffentliche medien- und kommunikationswissenschaftliche Forschung']).
+match([was,bringt,das,studium],['Durch dieses sehr empfehlenswerte Studium werden Sie auf folgende Berufsfelder vorbereitet:
+	Öffentlichkeitsarbeit,
+	(Fach)Journalismus,
+	Verlage,
+	Markt-und Medienforschung,
+	Medienkonzeption und -produktion
+	Medienmanagement,
+	und öffentliche medien- und kommunikationswissenschaftliche Forschung']).
+match([welche,_,habe,_,nach,dem,studium],['Durch dieses sehr empfehlenswerte Studium werden Sie auf folgende Berufsfelder vorbereitet:
+	Öffentlichkeitsarbeit,
+	(Fach)Journalismus,
+	Verlage,
+	Markt-und Medienforschung,
+	Medienkonzeption und -produktion
+	Medienmanagement,
+	und öffentliche medien- und kommunikationswissenschaftliche Forschung']).
 %Anfrage: wer bist du? %easter egg
+
+%Allgemeine Infos zum Studium
+match([wie,viele,semester,_,_,_],['Im Bachelor gibt es eine Regelstudienzeit von 6 Semester.']).
+match([wie,lange,dauert,das,studium],['Im Bachelor gibt es eine Regelstudienzeit von 6 Semester.']).
+match([wann,_,das,studium],['Das Studium der Medienwissenschaft können Sie jedes Wintersemester an der Eliteuniversität Tübingen beginnen.']).
+match([was,ist,der,nc],['Da der Vergabe der Studienplätze ein hochkomplexes Verfahren unterliegt kann ich diese Frage nicht beantworten.']).
 
 %Info zur Bib
 match([wann,ist,die,X,geoeffnet],['Die',X,hat,folgende,'Öffnungszeiten: ',Y]):- bib(X,Y).
@@ -104,7 +148,7 @@ match([wann,ist,die,X,geoeffnet],['Die',X,hat,folgende,'Öffnungszeiten: ',Y]):-
 match([info,ansprechpartner],[mewi,hat,folgende,ansprechpartner,':',ansprechp]).
 
 %Informationen zu Vorlesungen und Seminaren
-%vielleicht eine Antwort -> die <Eingabge> gibt es gar nicht bei MeWi, bei Fehleingabe
+%vielleicht eine Antwort -> die <Eingabe> gibt es gar nicht bei MeWi, bei Fehleingabe
 
 match([wie,viele,ects,punkte,bekommt,man,fuer,die,X],['Die', X, gibt, Y]):-
 	vorlesung(_,X,Y);seminar(_,X,Y);lehrredaktion(_,X,Y).
